8d988b849c94a287e4c56ad001ef2663ca921333
ajfldkvmek <146968265+ajfldkvmek@users.noreply.github.com>
2023-11-29 20:10:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.56 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+	
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        //3*2 2*3 --> 3*3 >> arr1의 row, arr2의 col
+        //arr1의 로우리스트 저장용
+        List<List<Integer>> row1 = new ArrayList<List<Integer>>();
+        
+        //arr2의 칼럼리스트 저장용
+        List<List<Integer>> col1 = new ArrayList<List<Integer>>();
+        //arr1 rowList
+        //arr2 colList
+//        [row][col]
+        //arr.length << row
+        //arr[n].length << col
+        //rowlist
+        
+        for(int row = 0; row < arr1.length  ;row++) {
+        	List<Integer> list = new ArrayList<Integer>();
+        	//저장시작
+        	for(int a = 0; a < arr1[row].length; a++) {
+        		list.add(arr1[row][a]);
+        	}
+        	row1.add(list);
+        }
+        
+        //칼럼은 고정 로우를 계속 증가후 가 끝내면 다시 칼럼증가
+        for(int col = 0; col < arr2[0].length; col++) {
+        	List<Integer> list = new ArrayList<Integer>();
+        	for(int a = 0; a < arr2.length; a++) {
+        		list.add(arr2[a][col]);
+        	}
+        	col1.add(list);
+        }
+        int n  = row1.get(0).size();
+        for(int a = 0; a < row1.size(); a++) {
+        	for(int b = 0; b < col1.size(); b++) {
+//        		row1.get(a) , col1.get(b) 의 숫잘들 각각 곱해서 answer[a][b] 에 삽입
+        		int sum = 0;
+        		for(int i = 0; i < n; i++) {
+        			sum += row1.get(a).get(i)*col1.get(b).get(i);
+        		}
+        		answer[a][b] = sum;
+        	}
+        }
+        
+        return answer;
+    }
+}
